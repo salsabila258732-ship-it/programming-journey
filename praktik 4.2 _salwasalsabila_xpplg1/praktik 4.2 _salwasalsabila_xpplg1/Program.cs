d0e50ff3637f8061acec1e86ae617db6b19d5593
salsabila258732-ship-it <salsabila258732@student.smkn1kandeman.sch.id>
2025-10-13 13:10:19 +0700

    Add files via upload

---
+++ praktik 4.2 _salwasalsabila_xpplg1/praktik 4.2 _salwasalsabila_xpplg1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik_4._2__salwasalsabila_xpplg1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan umur anda: ");
+            int umur = int.Parse(Console.ReadLine());
+
+            if (umur >= 18) ;
+            {
+
+                Console.WriteLine("Anda sudah cukup umur untuk membuat KTP.");
+
+
+
+
+
+
+
+            }
+        }
+    }
+}
