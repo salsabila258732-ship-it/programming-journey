2203d81dc864fab3133d5bf38c15dcebddd8ae04
salsabila258732-ship-it <salsabila258732@student.smkn1kandeman.sch.id>
2025-10-13 13:09:45 +0700

    Add files via upload

---
+++ praktik 4.1 mengecek angka positif/praktik 4.1 mengecek angka positif/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik_4._1_mengecek_angka_positif
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan angka: ");
+            int angka = int.Parse(Console.ReadLine());
+
+            if (angka > 0)
+            {
+                Console.WriteLine("Angka positif.");
+
+
+
+
+            }
+        }
+    }
+}
