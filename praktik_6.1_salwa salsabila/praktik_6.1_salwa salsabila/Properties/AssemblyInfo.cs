74d67ee4d191d1c02ba2d90132767a877964baa3
salsabila258732-ship-it <salsabila258732@student.smkn1kandeman.sch.id>
2025-11-03 14:54:34 +0700

    Add files via upload

---
+++ praktik_6.1_salwa salsabila/praktik_6.1_salwa salsabila/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("praktik_6.1_salwa salsabila")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("praktik_6.1_salwa salsabila")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0769ebc4-a984-4c29-b7ef-4fd22d12586c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ praktik_6.2_salwa salsabila x pplg 1/praktik_6.2_salwa salsabila x pplg 1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik_6._2_salwa_salsabila_x_pplg_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Membuat array integer dengan 5 elemen 
+            int[] angka = new int[5];
+
+            //Input elemen array dari keyboard
+            Console.Write("Masukkan nilai ke-1:");
+            angka[0] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Masukkan nilai ke-2:");
+            angka[1] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Masukkan nilai ke-3:");
+            angka[2] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Masukkan nilai ke-4:");
+            angka[3] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Masukkan nilai ke-5:");
+            angka[4] = Convert.ToInt32(Console.ReadLine());
+
+            //Menampilkan isi array tanpa perulangan 
+            Console.WriteLine("\nData yang Anda masukkan:");
+            Console.WriteLine("Nilai ke-1: " + angka[0]);
+            Console.WriteLine("Nilai ke-2: " + angka[1]);
+            Console.WriteLine("Nilai ke-3: " + angka[2]);
+            Console.WriteLine("Nilai ke-4: " + angka[3]);
+            Console.WriteLine("Nilai ke-5: " + angka[4]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
